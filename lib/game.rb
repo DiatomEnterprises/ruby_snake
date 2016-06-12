97a619a2890883a7662807ae127a6ee6ae580d96
Alexandr Kovtunov <1sasha.sasha1@gmail.com>
2016-06-12 19:32:28 +0300

    Added key-reader function

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'io/console'
 class Game
   attr_reader :gameboard, :snake
   def initialize
@@ -37,10 +38,40 @@ class Game
     tick
   end
 
+
+
+  # Reads keypresses from the user including 2 and 3 escape character sequences.
+  def read_char
+    STDIN.echo = false
+    STDIN.raw!
+
+    input = STDIN.getc.chr
+    if input == "\e" then
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+  ensure
+    STDIN.echo = true
+    STDIN.cooked!
+
+    return input
+  end
+
   def tick
     while true
       show
       sleep(0.1)
+      c = read_char
+
+      case c
+      when "\e"
+        puts "ESCAPE"
+      when /^.$/
+        puts "SINGLE CHAR HIT: #{c.inspect}"
+      else
+        puts "SOMETHING ELSE: #{c.inspect}"
+      end
+
       snake.step
     end
   end
