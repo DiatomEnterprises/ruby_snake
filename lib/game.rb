d4d6d137d609fa9bb311552df7d9689d8c5eb92e
Alexandr Kovtunov <1sasha.sasha1@gmail.com>
2016-08-18 01:09:04 +0300

    Copied code for second player, should be refactored using array of snakes

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,17 @@
 require 'io/console'
 class Game
-  attr_reader :gameboard, :snake, :food
+  attr_reader :gameboard, :snake, :snake2, :food
   def initialize(max_x=11, max_y=11)
     @gameboard = Board.new(max_x, max_y)
     @snake = Snake.new(gameboard.width, gameboard.length)
+    @snake2 = Snake.new(gameboard.width, gameboard.length)
     @food = Food.new(gameboard.width, gameboard.length)
   end
 
   def print_board
     system('clear')
     puts "Your size is: #{snake.size} |  [Q]uit"
+    puts "Your opponent's size is: #{snake2.size} "
     puts "---------------------------"
     gameboard.board.each do
       |line| puts line.each{|item| item}.join(" ")
@@ -20,6 +22,9 @@ class Game
     gameboard.create_board
     @gameboard.board[food.x][food.y] = 'o'
     snake.parts.each do |part|
+      @gameboard.board[part.first][part.last] = 'x'
+    end
+    snake2.parts.each do |part|
       @gameboard.board[part.first][part.last] = 'x'
     end
     print_board
@@ -71,11 +76,20 @@ class Game
     snake.update_head(1, gameboard.width-1) if snake.head[1] < 0
     snake.update_head(0, 0) if snake.head[0]  > gameboard.length-1
     snake.update_head(0, gameboard.length-1) if snake.head[0] < 0
+
+    snake2.update_head(1,0) if snake2.head[1] > gameboard.width-1
+    snake2.update_head(1, gameboard.width-1) if snake2.head[1] < 0
+    snake2.update_head(0, 0) if snake2.head[0]  > gameboard.length-1
+    snake2.update_head(0, gameboard.length-1) if snake2.head[0] < 0
   end
 
   def check_if_snake_ate_food
     if snake.head[0] == food.x && snake.head[1] == food.y
       snake.increase
+      @food = Food.new(gameboard.width, gameboard.length)
+    end
+    if snake2.head[0] == food.x && snake2.head[1] == food.y
+      snake2.increase
       @food = Food.new(gameboard.width, gameboard.length)
     end
   end
@@ -99,6 +113,7 @@ class Game
         in_game = execute_action(key)
       end
       snake.step
+      snake2.step
       check_snake_position
     end
     show_message("Game quit")
@@ -107,6 +122,7 @@ class Game
   def execute_action key
     return false if compare_key(key, 'q')
     snake.turn(key)
+    snake2.turn(['q','w','s','d'][Random.rand(4)])
   end
 
   def compare_key(key, char)
